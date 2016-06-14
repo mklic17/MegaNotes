42b37275722b9b28663669402b62a3921fdef228
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-14 15:12:32 -0400

    notes are added to the sidebar. also Eslint was placed into the proper file.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,43 +1,46 @@
 (function() {
-
-
-    angular.module('meganote.notes', ['ui.router'])
+  angular.module('meganote.notes', ['ui.router'])
     .config(notesConfig)
     .controller('NotesController', NotesController); //Passes the second NotesController function as an object
 
-    notesConfig.$inject= ['$stateProvider'];//contains all of the dependencies as strings
-    function notesConfig($stateProvider) { //now $stateProvider does not have to be called that if you do not want it to be
-      $stateProvider
+  notesConfig.$inject= ['$stateProvider'];//contains all of the dependencies as strings
+  function notesConfig($stateProvider) { //now $stateProvider does not have to be called that if you do not want it to be
+    $stateProvider
 
       .state('notes', {
-          url: '/notes',
-          templateUrl: 'notes/notes.html',
-          controller: 'NotesController'
-        })
+        url: '/notes',
+        templateUrl: 'notes/notes.html',
+        controller: 'NotesController'
+      })
 
       .state('notes.form', {
         url: '/:noteId',
         templateUrl: 'notes/notes-form.html'
       });
-    }
+  }
 
-    NotesController.$inject = ['$state', '$scope'];
-    function NotesController($state,$scope) {
-      $state.go('notes.form')
+  NotesController.$inject = ['$state', '$scope', 'NotesService'];
+  function NotesController($state, $scope, NotesService) {
+    $state.go('notes.form');
 
-      $scope.notes = []; //The actual notes on the sidebar
-      $scope.note = {title: '', body: ''}; //Empty object, the form's title and body
+    NotesService.getNotes() //not global so we need to inject it
+        .then(function() {
+          $scope.notes = NotesService.notes; //The actual notes on the sidebar
+        });
 
-      $scope.addNote = function(){
-        $scope.notes.push($scope.note);
-        $scope.note = {}; //What is in the form
-      }
-      $scope.removeNote = function(index){
-        $scope.notes.splice(index, 1);
-      }
-      $scope.editNote = function(note){
-        $scope.note = note;
-      }
-    }
+
+    $scope.note = {title: '', body: ''}; //Empty object, the form's title and body
+
+    $scope.addNote = function(){
+      $scope.notes.push($scope.note);
+      $scope.note = { title: '', body: ''}; //What is in the form
+    };
+    $scope.removeNote = function(index){
+      $scope.notes.splice(index, 1);
+    };
+    $scope.editNote = function(note){
+      $scope.note = note;
+    };
+  }
 
 }());
